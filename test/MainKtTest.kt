efc79a4be9be3aa7bc89c59075ffaf58951b16f2
Kelly Yang <kellyyang224@gmail.com>
2024-01-25 17:27:36 -0500

    Fixed formula

--- test/MainKtTest.kt
+++ test/MainKtTest.kt
@@ -4,7 +4,13 @@ class MainKtTest {
     fun converter() {
         val convertedFahrenheit = converter(30.0, "Celsius")
         val incorrectValue = 123123.0
+        val correctValue = 86.0
 
+        //Test that incorrect value returns True
         assert(convertedFahrenheit != incorrectValue)
+
+        //Test that correct value returns True
+        assert(convertedFahrenheit == correctValue)
+
     }
 }
