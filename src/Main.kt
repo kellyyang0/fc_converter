e3b83bf857fa3ababb85662a41bae82938eade99
Kelly Yang <kellyyang224@gmail.com>
2024-01-25 17:27:16 -0500

    Fixed formula

--- src/Main.kt
+++ src/Main.kt
@@ -3,14 +3,14 @@ fun main() {
     val measurement: String
 
     // Ask user which conversion is wanted
-    print("Type F2C for Fahrenheit to Celcius and C2F for Celcius to Fahrenheit")
+    print("Type F2C for Fahrenheit to Celsius and C2F for Celsius to Fahrenheit")
     val enteredWay: String = readln()
 
     // Initialize measurement variable with correct type
     measurement = if (enteredWay == "C2F") {
         "Fahrenheit"
     } else {
-        "Celcius"
+        "Celsius"
     }
 
     // Ask user to input starting temperature
@@ -18,24 +18,26 @@ fun main() {
     val enteredNum: Double = readln().toDouble()
 
     // Call converter function and store into variable
-    val convertedNum = converter(enteredNum, measurement)
+    val convertedNum: Double = converter(enteredNum, measurement)
 
     // Print final conversion
     print("$enteredNum $measurement is $convertedNum")
 }
 
 /**
- * Converts Fahrenheit to Celcius and vice-versa
+ * Converts Fahrenheit to Celsius and vice-versa
  *
  * @param orgNum value to be converted
- * @param corF desired target unit
- * @return [orgNum] converted to the unit given in [corF]
+ * @param cOrF desired target unit
+ * @return [orgNum] converted to the unit given in [cOrF]
  */
-fun converter(orgNum: Double, corF: String): Double {
-    return if (corF == "Celcius") {
-        ((5 / 9) * orgNum) + 32
+fun converter(orgNum: Double, cOrF: String): Double {
+    return if (cOrF == "Celsius") {
+        //Fahrenheit to Celsius conversion formula
+        (orgNum - 32) * (5.0 / 9.0)
     } else {
-        ((9 / 5) * orgNum) - 32
+        //Celsius to Fahrenheit conversion formula
+        (orgNum * (9.0 / 5.0)) + 32
     }
 }
 
